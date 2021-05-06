d255ec0408ea313c7e306f6f2376e9f632c12c46
VasiliyMac <cartario@yandex.ru>
2021-05-06 16:42:59 +0300

    viroPropApp

--- App.js
+++ App.js
@@ -1,126 +1,127 @@
 import React from 'react';
-import {  
-  Text,
-  View,
-  StyleSheet,  
-  TouchableHighlight,
-} from 'react-native';
+import { Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 
-import {
-  ViroVRSceneNavigator,
-  ViroARSceneNavigator
-} from 'react-viro';
+import { ViroARSceneNavigator } from 'react-viro';
 
 import InitialARScene from './js/HelloWorldSceneAR';
-import InitialVRScene from './js/HelloWorldScene'
+import ToyScene from './src/screens/ar-screen1';
 
-const NAVIGATOR_TYPES = {  
+import LoginScreen from './src/screens/Login';
+
+const NAVIGATOR_TYPES = {
   screen1: 'AR',
-  screen2: 'VR'
-}
+  screen2: 'VR',
+  login: 'login',
+};
 
-const WelcomeScreen = ({onSelectScreen}) => { 
-
+const WelcomeScreen = ({ navigateTo }) => {
   return (
-  <View style={localStyles.outer} >
-    <View style={localStyles.inner} >
+    <View style={localStyles.outer}>
+      <View style={localStyles.inner}>
+        <Text style={localStyles.titleText}>Выберите режим:</Text>
 
-      <Text style={localStyles.titleText}>
-        Выберите режим:
-      </Text>
+        <TouchableHighlight
+          style={localStyles.buttons}
+          onPress={() => navigateTo(NAVIGATOR_TYPES.screen1)}
+          underlayColor={'#68a0ff'}
+        >
+          <Text style={localStyles.buttonText}>По рецепту</Text>
+        </TouchableHighlight>
 
-      <TouchableHighlight style={localStyles.buttons}
-        onPress={()=>onSelectScreen(NAVIGATOR_TYPES.screen1)}
-        underlayColor={'#68a0ff'} >
+        <TouchableHighlight
+          style={localStyles.buttons}
+          onPress={() => navigateTo(NAVIGATOR_TYPES.screen2)}
+          underlayColor={'#68a0ff'}
+        >
+          <Text style={localStyles.buttonText}>Свободное посещение</Text>
+        </TouchableHighlight>
 
-        <Text style={localStyles.buttonText}>По рецепту</Text>
-      </TouchableHighlight>
-
-      <TouchableHighlight style={localStyles.buttons}        
-        onPress={()=>onSelectScreen(NAVIGATOR_TYPES.screen2)}
-        underlayColor={'#68a0ff'} >
-
-        <Text style={localStyles.buttonText}>Свободное посещение</Text>
-      </TouchableHighlight>
+        <TouchableHighlight
+          style={localStyles.buttons}
+          onPress={() => navigateTo(NAVIGATOR_TYPES.login)}
+          underlayColor={'#68a0ff'}
+        >
+          <Text style={localStyles.buttonText}>Зарегистрироваться</Text>
+        </TouchableHighlight>
+      </View>
     </View>
-  </View>)
-}
+  );
+};
 
-export default function App () {
-  const [navigatorType, setNavigatorType] = React.useState(null)
+export default function App() {
+  const [navigatorType, setNavigatorType] = React.useState(null);
 
   const _navigateTo = (navigatorType) => {
-    setNavigatorType(navigatorType)
-  }
+    setNavigatorType(navigatorType);
+  };
 
   const _getHome = () => {
-    setNavigatorType(null)
-  }
+    setNavigatorType(null);
+  };
 
-  switch(navigatorType){
+  switch (navigatorType) {
     case NAVIGATOR_TYPES.screen1:
-      return <ViroARSceneNavigator
-      initialScene={{scene: InitialARScene}} />
+      return <ViroARSceneNavigator initialScene={{ scene: InitialARScene }} />;
     case NAVIGATOR_TYPES.screen2:
-      return <ViroVRSceneNavigator
-      initialScene={{scene: InitialVRScene}} />
+      return <ViroARSceneNavigator initialScene={{ scene: ToyScene }} viroAppProps={{goBack: _getHome, navigateTo: _navigateTo}}/>;
+    case NAVIGATOR_TYPES.login:
+      return <LoginScreen goBack={_getHome}/>;
     default:
-      return (<WelcomeScreen onSelectScreen={_navigateTo}/>)
-  }  
+      return <WelcomeScreen navigateTo={_navigateTo} />;
+  }
 }
 
 var localStyles = StyleSheet.create({
-  viroContainer :{
-    flex : 1,
-    backgroundColor: "black",
+  viroContainer: {
+    flex: 1,
+    backgroundColor: 'black',
   },
-  outer : {
-    flex : 1,
+  outer: {
+    flex: 1,
     flexDirection: 'row',
-    alignItems:'center',
-    backgroundColor: "black",
+    alignItems: 'center',
+    backgroundColor: 'black',
   },
   inner: {
-    flex : 1,
+    flex: 1,
     flexDirection: 'column',
-    alignItems:'center',
-    backgroundColor: "black",
+    alignItems: 'center',
+    backgroundColor: 'black',
   },
   titleText: {
     paddingTop: 30,
     paddingBottom: 20,
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 25
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 25,
   },
   buttonText: {
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 20
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
   },
-  buttons : {
+  buttons: {
     height: 80,
     width: '80%',
-    paddingTop:20,
-    paddingBottom:20,
+    paddingTop: 20,
+    paddingBottom: 20,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor:'#68a0cf',
+    backgroundColor: '#68a0cf',
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
   },
-  exitButton : {
+  exitButton: {
     height: 50,
     width: 100,
-    paddingTop:10,
-    paddingBottom:10,
+    paddingTop: 10,
+    paddingBottom: 10,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor:'#68a0cf',
+    backgroundColor: '#68a0cf',
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
-  }
+  },
 });
-
---
+++ src/screens/ar-screen1.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import InitialARScene from '../../js/HelloWorldSceneAR';
+
+import {  
+  ViroARScene,
+  ViroText,
+  ViroAmbientLight,
+  ViroFlexView,
+  ViroMaterials
+} from 'react-viro';
+
+export default function Screen1 ({sceneNavigator}) {
+  const [count, setCount] = React.useState(3);
+
+  const handleClick = () => {
+    // sceneNavigator.jump({scene: InitialARScene})
+    // sceneNavigator.viroAppProps.goBack()
+
+    sceneNavigator.viroAppProps.navigateTo('login')
+    // setCount(()=>{
+    //   return count + 1;
+    // })
+  }
+
+  return (
+    <ViroARScene>  
+      <ViroAmbientLight color="#ffffff" />    
+      <ViroText 
+      onClick={handleClick}      
+      text="На главную" width={2} height={2} position={[0, -3, -7]} style={styles.helloWorldTextStyle} />
+
+      <ViroFlexView
+          style={styles.titleContainer}
+          position={[0, 1, -7]}
+          rotation={[0, 0, 0]}
+          height={6}
+          width={12}
+          // onDrag={() => {}}
+          materials={['screen2']}
+        >
+          
+        </ViroFlexView>
+    </ViroARScene>   
+  );
+}
+
+ViroMaterials.createMaterials({
+  screen2: {
+    diffuseTexture: require('../../js/res/screen2.jpeg'),
+  },  
+});
+
+const styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 60,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',  
+  },
+  titleContainer: {
+    flexDirection: 'column',
+    padding: 0.2,
+    backgroundColor: '#ffffffdd',
+  },
+});
