556e5ef1236066eb907b896736cb9b70c7237bae
VasiliyMac <cartario@yandex.ru>
2021-05-16 12:22:25 +0300

    firebaseAdapter

--- src/screens/HelloWorldSceneAR.js
+++ src/screens/HelloWorldSceneAR.js
@@ -20,44 +20,48 @@ import {
 
 import useHttp from '../hooks/useHttp';
 
-import {NAVIGATOR_TYPES} from '../utils'
+import { NAVIGATOR_TYPES, fireBaseAdapter } from '../utils';
 
-const BASE_URL =
+const PRODUCTS_URL =
   'https://virtual-shoping-b52fd-default-rtdb.europe-west1.firebasedatabase.app/products.json';
 
-export default function Screen1({sceneNavigator}) {
+const ORDERS_URL =
+  'https://virtual-shoping-b52fd-default-rtdb.europe-west1.firebasedatabase.app/orders.json';
+
+export default function Screen1({ sceneNavigator }) {
   const [text, setText] = React.useState('loading');
   const [spinner, setSpinner] = React.useState(true);
+  const [data, setData] = React.useState(null);
 
-  const {request, loading} = useHttp();
-  
+  const { request, loading } = useHttp();
+
   const _onInitialized = (state, reason) => {
     if (state == ViroConstants.TRACKING_NORMAL) {
       setText('ХАКАТОН ВТБ');
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
-  }
+  };
 
   const handleHomeMode = async () => {
-    const data = await request(BASE_URL);
-    setText(JSON.stringify(data));    
-    sceneNavigator.viroAppProps.navigateTo(NAVIGATOR_TYPES.arProductInfo)
-  }
+    const data = await request(ORDERS_URL);
+    setData(data);
+    // setText(JSON.stringify(data));
+
+    // sceneNavigator.viroAppProps.navigateTo(NAVIGATOR_TYPES.arProductInfo)
+  };
 
   const handleBackgroundLoaded = () => {
-    setSpinner(false)
-  }
+    setSpinner(false);
+  };
+
+  const allOrders = fireBaseAdapter(data);
 
   return (
     <ViroARScene onTrackingUpdated={_onInitialized}>
       <ViroAmbientLight color="#ffffff" />
 
-      <ViroSpinner
-        visible={spinner}
-        type="Light"
-        position={[0, 0, -2.5]}
-      />
+      <ViroSpinner visible={spinner} type="Light" position={[0, 0, -2.5]} />
 
       <Viro360Image
         format="RGBA8"
@@ -68,89 +72,100 @@ export default function Screen1({sceneNavigator}) {
       />
 
       <ViroFlexView
-          style={styles.titleContainer}
-          position={[0, 3.5, -7]}
-          rotation={[10, 0, 0]}
-          height={2}
-          width={4}
-          onClick={handleHomeMode}
-        >
-          <ViroText
+        style={styles.titleContainer}
+        position={[0, 3.5, -7]}
+        rotation={[10, 0, 0]}
+        height={2}
+        width={4}
+        onClick={handleHomeMode}
+      >
+        {/* <ViroText
             style={styles.prodTitleText}
             text={'загрузить данные с сервера'}
             width={4}
             height={0.5}
-          />
-        </ViroFlexView>
+          /> */}
 
-        <ViroNode
-          position={[-1, 0, -0.5]}
-          dragType="FixedToWorld"
-          
-          onPinch={() => {}}
-        >
-          <ViroSpotLight
-            innerAngle={5}
-            outerAngle={45}
-            direction={[0, -1, -0.2]}
-            position={[0, 3, 0]}
-            color="#ffffff"
-            castsShadow={true}
-            influenceBitMask={4}
-            shadowMapSize={2048}
-            shadowNearZ={2}
-            shadowFarZ={5}
-            shadowOpacity={0.7}
-          />
+        {data ? (
+          allOrders[0].products.map((product, i) => {
+            return (
+              <ViroText
+                key={i}
+                style={styles.prodTitleText}
+                text={product.productId}
+                width={4}
+                height={0.5}
+              />
+            );
+          })
+        ) : (
+          <ViroText style={styles.prodTitleText} text={'loading...'} width={4} height={0.5} />
+        )}
+      </ViroFlexView>
 
-          <Viro3DObject
-            materials={['shelf']}
-            source={require('../res/shelf/model.obj')}
-            position={[2.5, -1.65, -2]}
-            rotation={[0, 90, 0]}
-            scale={[0.06, 0.06, 0.06]}
-            type="OBJ"
-            lightReceivingBitMask={5}
-            shadowCastingBitMask={4}
-          />
+      <ViroNode position={[-1, 0, -0.5]} dragType="FixedToWorld" onPinch={() => {}}>
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={45}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 0]}
+          color="#ffffff"
+          castsShadow={true}
+          influenceBitMask={4}
+          shadowMapSize={2048}
+          shadowNearZ={2}
+          shadowFarZ={5}
+          shadowOpacity={0.7}
+        />
 
-          <Viro3DObject
-            materials={['milk']}
-            source={require('../res/milk/milk.obj')}
-            position={[2, -0.5, -2]}
-            scale={[0.01, 0.01, 0.01]}
-            type="OBJ"
-            lightReceivingBitMask={5}
-            shadowCastingBitMask={4}
-            onDrag={() => {}}
-          />
+        <Viro3DObject
+          materials={['shelf']}
+          source={require('../res/shelf/model.obj')}
+          position={[2.5, -1.65, -2]}
+          rotation={[0, 90, 0]}
+          scale={[0.06, 0.06, 0.06]}
+          type="OBJ"
+          lightReceivingBitMask={5}
+          shadowCastingBitMask={4}
+        />
 
-          <Viro3DObject
-            materials={['milk']}
-            source={require('../res/milk/milk.obj')}
-            position={[2.2, 0.1, -1.6]}
-            scale={[0.01, 0.01, 0.01]}
-            type="OBJ"
-            lightReceivingBitMask={5}
-            shadowCastingBitMask={4}
-            onDrag={() => {}}
-          />
+        <Viro3DObject
+          materials={['milk']}
+          source={require('../res/milk/milk.obj')}
+          position={[2, -0.5, -2]}
+          scale={[0.01, 0.01, 0.01]}
+          type="OBJ"
+          lightReceivingBitMask={5}
+          shadowCastingBitMask={4}
+          onDrag={() => {}}
+        />
+
+        <Viro3DObject
+          materials={['milk']}
+          source={require('../res/milk/milk.obj')}
+          position={[2.2, 0.1, -1.6]}
+          scale={[0.01, 0.01, 0.01]}
+          type="OBJ"
+          lightReceivingBitMask={5}
+          shadowCastingBitMask={4}
+          onDrag={() => {}}
+        />
       </ViroNode>
 
       <ViroText
-          onDrag={() => {}}
-          text={text}
-          scale={[0.5, 0.5, 0.5]}
-          position={[0, 2.5, -4]}
-          style={styles.helloWorldTextStyle}
-        />
-        <ViroBox
-          position={[-1.5, -2, -3]}
-          rotation={[0, 20, 0]}
-          scale={[1, 1, 1]}
-          materials={['vtb']}
-          onDrag={() => {}}
-        />
+        onDrag={() => {}}
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 2.5, -4]}
+        style={styles.helloWorldTextStyle}
+      />
+      <ViroBox
+        position={[-1.5, -2, -3]}
+        rotation={[0, 20, 0]}
+        scale={[1, 1, 1]}
+        materials={['vtb']}
+        onDrag={() => {}}
+      />
     </ViroARScene>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,19 @@ export const NAVIGATOR_TYPES = {
   login: 'login',
   arProductInfo: 'arProductInfo'
 };
+
+export const fireBaseAdapter = (response) => {
+  if (!response) {
+    return;
+  }
+  const keys = Object.keys(response);
+
+  return keys.map((key) => {
+    return {
+      _id: key,
+      ...response[key],
+    };
+  });
+};
+
+
